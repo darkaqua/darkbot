e87c6616f6b46cab7327bd7e0222d0e146863a75
mcmacker4 <mcmacker4@gmail.com>
2017-03-05 22:53:34 +0100

    Comando !ping devuelve el ping

--- bot/commands/cm_ping.js
+++ bot/commands/cm_ping.js
@@ -2,7 +2,7 @@ module.exports = {
     roles: ["@everyone"],
     descr: "Pong!",
     exec: (message) => {
-        message.author.sendMessage("Pong!");
+        message.author.sendMessage(`Pong! (${message.author.client.ping}ms)`);
         message.delete();
     }
 }
