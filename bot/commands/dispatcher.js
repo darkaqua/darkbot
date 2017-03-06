4c4d4ebe3cf557bbb891185405e23393d13c838c
mcmacker4 <mcmacker4@gmail.com>
2017-03-06 13:27:38 +0100

    Arreglado el prefix del comando

--- bot/commands/dispatcher.js
+++ bot/commands/dispatcher.js
@@ -4,6 +4,7 @@ const commands = require("./commands.js");
 module.exports = function(message) {
     let content = message.content;
     let parts = content.split(" ");
+    parts[0] = parts[0].substring(1, parts[0].length);
     if(commands.list.hasOwnProperty(parts[0])) {
         let command = commands.list[parts[0]];
         if(commands.hasPermission(command, message.member)) {
