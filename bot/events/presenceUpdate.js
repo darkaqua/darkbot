6f2aedef6ab88e2b8c6504a7ac2681413be95499
mcmacker4 <mcmacker4@gmail.com>
2017-03-11 23:11:04 +0100

    Jugadores del League of Loosers
    Ahora se les añade el rol [SALT] a los jugadores del LoL automaticamente

---
+++ bot/events/presenceUpdate.js
@@ -0,0 +1,9 @@
+module.exports = (oldMember, newMember) => {
+
+    //Si el miembro se ha puesto a jugar al lol
+    if(newMember.presence.game && newMember.presence.game.name === "League of Legends") {
+        //Le ponemos el rol [SALT]
+        newMember.addRole(newMember.guild.roles.find("name", "[SALT]"));
+    }
+
+}
