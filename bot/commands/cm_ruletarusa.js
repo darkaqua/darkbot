4aadc95fd0b37702d0f2d4bbf1e93184cd141ce3
The MagicInventor <magicinventor@hotmail.com>
2017-03-19 11:25:24 -0300

    less code
    single-line if

--- bot/commands/cm_ruletarusa.js
+++ bot/commands/cm_ruletarusa.js
@@ -2,13 +2,7 @@ module.exports = {
     roles: ["@everyone"],
     descr: "¿Le temes a la muerte?",
     exec: (message, args) => {
-        message.reply(ruleta()).then(msg => msg.delete(5000));
+        message.reply(Math.random() >= 0.5 ? "¡Pum! Muerto" : "La suerte te acompaña...").then(msg => msg.delete(5000));
         message.delete();
     }
-}
-const ruleta = () => {
-    if(Math.floor(Math.random() * 20 % 6) === 1){
-        return "¡Pum! Muerto"
-    }
-    return "La suerte te acompaña..."
 }
