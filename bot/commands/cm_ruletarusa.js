9847f213b88892510a2c0568fd6127dbdd6cb34c
The MagicInventor <magicinventor@hotmail.com>
2017-03-19 17:31:53 -0300

    less code
    ...

--- bot/commands/cm_ruletarusa.js
+++ bot/commands/cm_ruletarusa.js
@@ -2,7 +2,7 @@ module.exports = {
     roles: ["@everyone"],
     descr: "¿Le temes a la muerte?",
     exec: (message, args) => {
-        message.reply(Math.random() >= 0.5 ? "¡Pum! Muerto" : "La suerte te acompaña...").then(msg => msg.delete(5000));
+        message.reply(Math.floor(Math.random() * 20 % 6) === 1 ? "¡Pum! Muerto" : "La suerte te acompaña...").then(msg => msg.delete(5000));
         message.delete();
     }
 }
