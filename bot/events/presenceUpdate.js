e20f33eb096c1f301b692985d2e795cb9edfa9a9
TheCreativeGod <adrianbeltran_1998@hotmail.com>
2017-04-20 21:45:45 -0700

    Role name, role remove.
    - Detectar cuando el usuario deja de stremear
    - Cambio del nombre del rol, de `[streamer]` a `[streaming]`

--- bot/events/presenceUpdate.js
+++ bot/events/presenceUpdate.js
@@ -5,12 +5,18 @@ module.exports = (oldMember, newMember) => {
         //Le ponemos el rol [SALT]
         newMember.addRole(newMember.guild.roles.find("name", "[salt]"));
     //Si el miembro está stremeando
-    } else if (newMember.presence.game && newMember.presence.game.streaming == true) {
-       //Le ponemos el rol [streamer]
-      newMember.addRole(newMember.guild.roles.find("name", "[streamer]"))
+  } else if (newMember.presence.game && newMember.presence.game.streaming === true) {
+       //Le ponemos el rol [streaming]
+      newMember.addRole(newMember.guild.roles.find("name", "[streaming]"))
       .catch(err =>
-				console.log(`${err.message} (join [streamer])`)
+				console.log(`${err.message} (join [streaming])`)
 			);
+    //Si el miembro dejó de estremear
+  } else if (oldMember.presence.game && oldMember.presence.game.streaming === true && ((newMember.presence.game && newMember.presence.game.streaming === false) || !newMember.presence.game)) {
+      //Le quitamos el rol [streaming]
+      newMember.removeRole(newMember.guild.roles.find("name", "[streaming]"))
+      .catch(err =>
+        console.log(`${err.message} (leave [streaming])`)
+      );
     }
-
 }
