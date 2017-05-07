77fc132013012c996a30a3ad601eb87b0323a097
Pablo González <pagoru@users.noreply.github.com>
2017-05-07 20:30:12 +0200

    Update presenceUpdate.js

--- bot/events/presenceUpdate.js
+++ bot/events/presenceUpdate.js
@@ -7,16 +7,16 @@ module.exports = (oldMember, newMember) => {
     //Si el miembro está stremeando
   } else if (newMember.presence.game && newMember.presence.game.streaming === true) {
        //Le ponemos el rol [streaming]
-      newMember.addRole(newMember.guild.roles.find("name", "[streaming]"))
+      newMember.addRole(newMember.guild.roles.find("name", "[twitch]"))
       .catch(err =>
-				console.log(`${err.message} (join [streaming])`)
+				console.log(`${err.message} (join [twitch])`)
 			);
     //Si el miembro dejó de estremear
   } else if (oldMember.presence.game && oldMember.presence.game.streaming === true && ((newMember.presence.game && newMember.presence.game.streaming === false) || !newMember.presence.game)) {
       //Le quitamos el rol [streaming]
-      newMember.removeRole(newMember.guild.roles.find("name", "[streaming]"))
+      newMember.removeRole(newMember.guild.roles.find("name", "[twitch]"))
       .catch(err =>
-        console.log(`${err.message} (leave [streaming])`)
+        console.log(`${err.message} (leave [twitch])`)
       );
     }
 }
