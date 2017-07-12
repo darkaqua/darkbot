014eebfadb58bd9ba3c2d3d7655af7a46ca88aaf
Pablo González <pagoru@gmail.com>
2017-07-12 02:31:41 +0200

    Fixed - !prefix

--- bot/commands/cm_prefix.js
+++ bot/commands/cm_prefix.js
@@ -1,18 +1,19 @@
+const Discord = require('discord.js');
 const punct = [",", "!", "?", "@", "#", "$", "%", "^", "&", "*"];
 
 module.exports = {
-    roles: ["[admin]"],
+    roles: ["[boss]"],
     descr: "Cambiar el prefijo de los comandos.",
     exec: (message, args) => {
-        if(args[1] && args[1].length == 1 && punct.includes(args[1])) {
+        if(args[1] && args[1].length === 1 && punct.includes(args[1])) {
             global.config.prefix = args[1];
             message.guild.channels.find("name", "bienvenida")
-                    .sendMessage("@everyone El prefijo de los comandos se ha cambiado a " + args[1]);
+                    .send("@everyone El prefijo de los comandos se ha cambiado a " + args[1], null);
         } else {
-            message.author.sendEmbed({
-                description: "El prefijo tiene que ser uno de los siguientes caracteres: `" + punct.join(" ") + "`",
-                color: 2527667
-            });
+            const embed = new Discord.RichEmbed();
+            embed.setDescription(`El prefijo tiene que ser uno de los siguientes caracteres: \`${punct.join(' ')}\``);
+            embed.setColor(2527667);
+            message.author.send('', Discord.MessageOptions = { embed: embed });
         }
         message.delete();
     }
