7e6a0af035e4e489be6b5e500371ce13ed22d285
Pablo González <pagoru@gmail.com>
2017-07-12 02:15:42 +0200

    Fixed - !date

--- bot/commands/cm_date.js
+++ bot/commands/cm_date.js
@@ -1,12 +1,15 @@
+const Discord = require('discord.js');
+
 module.exports = {
     roles: ["@everyone"],
     descr: "Obtener la fecha actual en el servidor.",
     exec: (message, args) => {
         let date = new Date();
-        message.channel.sendEmbed({
-            description: "La fecha es `" + date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear() + "` en este servidor.",
-            color: 2527667
-        }, message.author.toString());
+        const embed = new Discord.RichEmbed();
+        embed.setDescription(`La fecha es \`${date.getDate()}/${date.getMonth()}/${date.getFullYear()}\` en este servidor.`);
+        embed.setColor(2527667);
+
+        message.channel.send(message.author.toString(), Discord.MessageOptions = { embed: embed });
         message.delete();
     }
 }
