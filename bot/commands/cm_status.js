bb7dc1cb062717d17ba26e5634a770cc72c26d86
Pablo González <pagoru@gmail.com>
2017-07-12 02:43:57 +0200

    Fixed - !status

--- bot/commands/cm_status.js
+++ bot/commands/cm_status.js
@@ -1,16 +1,19 @@
+const Discord = require("discord.js");
+
 const statuses = ["online", "idle", "invisible", "dnd"];
 
 module.exports = {
-    roles: ["[admin]"],
+    roles: ["[boss]"],
     descr: "Cambia el estado del bot.",
     exec: (message, args) => {
         if(statuses.includes(args[1])) {
             global.bot.user.setStatus(args[1]);
         } else {
-            message.author.sendEmbed({
-                description: "El estado debe ser `online`, `idle`, `invisible` o `dnd` (do not disturb).",
-                color: 2527667
-            });
+            const embed = new Discord.RichEmbed();
+            embed.setDescription("El estado debe ser `online`, `idle`, `invisible` o `dnd` (do not disturb).");
+            embed.setColor(2527667);
+
+            message.author.send('', Discord.MessageOptions = { embed: embed });
         }
         message.delete();
     }
